bfa00c4437687d2e7ad66af9f5ab271165bf0873
esalu-dev <esalu@profedex.top>
2024-12-02 19:27:26 -0600

    Add LoginForm component

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,10 @@
+import LoginForm from "@/components/login/LoginForm/LoginForm";
 
 export default function LoginPage() {
     return (
-        <main className="flex w-screen h-screen flex-row-reverse">
-            <section className="flex flex-1 justify-center items-center">
-                <h1 className="font-bold text-4xl">Iniciar sesión</h1>
-
-            </section>
-            <aside className="flex flex-1 relative">
-                <img src="/login.jpg" alt="Login background image" className="object-cover"/>
-                {/*<h2 className="absolute text-white top-32 left-32 text-6xl font-bold">¡Bienvenido de nuevo!</h2>*/}
-            </aside>
-        </main>
+        <main className="flex w-screen h-screen relative items-center justify-center">
+            <img src="/login.jpg" alt="Background image for login form" className='absolute w-screen h-screen object-cover'/>
+            <LoginForm />
+       </main>
    )
 }
