5fb8fc9be9890924554dc68b64c11da69959e56e
esalu-dev <esalu@profedex.top>
2024-12-03 19:35:29 -0600

    Finished authentication

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,28 +1,40 @@
 'use client'
 
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import {useState} from 'react'
+import {Button} from "@/components/ui/button"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card"
+import {EyeIcon, EyeOffIcon} from 'lucide-react'
+import {loginAction} from "@/actions/auth/loginAction";
 
 
 export default function LoginForm() {
     const [showPassword, setShowPassword] = useState(false)
+    const [error, setError] = useState(false)
 
     return (
         <Card className="w-full max-w-md z-10">
             <CardHeader>
                 <CardTitle className="text-2xl font-bold">¡Bienvenido de vuelta!</CardTitle>
                 <CardDescription>Ingresa tus credenciales para iniciar sesión</CardDescription>
+                {
+                    error && (
+                        <CardDescription className="text-red-500">Error iniciando sesión</CardDescription>
+                    )
+                }
             </CardHeader>
             <CardContent>
-                <form>
+                <form action={async (formData) => {
+                    const res = await loginAction(formData)
+                    if (res.error) {
+                        setError(true)
+                    }
+                }}>
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="email">Email</Label>
-                            <Input id="email" type="email" placeholder="linustorvalds@gmail.com" />
+                            <Input id="email" type="email" placeholder="linustorvalds@gmail.com" name="email"/>
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="password">Contraseña</Label>
@@ -31,6 +43,7 @@ export default function LoginForm() {
                                     id="password"
                                     type={showPassword ? "text" : "password"}
                                     placeholder="••••••••••••"
+                                    name="password"
                                 />
                                 <Button
                                     type="button"
@@ -41,19 +54,17 @@ export default function LoginForm() {
                                     aria-label={showPassword ? "Hide password" : "Show password"}
                                 >
                                     {showPassword ? (
-                                        <EyeOffIcon className="h-4 w-4" />
+                                        <EyeOffIcon className="h-4 w-4"/>
                                     ) : (
-                                        <EyeIcon className="h-4 w-4" />
+                                        <EyeIcon className="h-4 w-4"/>
                                     )}
                                 </Button>
                             </div>
                         </div>
+                        <Button className="w-full">Iniciar sesión</Button>
                     </div>
                 </form>
             </CardContent>
-            <CardFooter className="flex flex-col items-start gap-4">
-                <Button className="w-full">Iniciar sesión</Button>
-            </CardFooter>
         </Card>
     )
 }
