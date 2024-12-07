4091a7ad48d494e3629ae7b574dc0a1b0f8a3999
esalu-dev <esalu@profedex.top>
2024-12-07 17:51:26 -0600

    Add MyProjectsSection.tsx on Projects Page

--- src/app/(protected)/app/projects/page.tsx
+++ src/app/(protected)/app/projects/page.tsx
@@ -2,7 +2,7 @@ import {MyProjectsSection} from "@/components/app/projects/MyProjectsSection";
 
 export default function ProjectsPage() {
     return (
-        <div className='outline h-full flex-1 w-full p-5'>
+        <div className='h-full flex-1 w-full p-5'>
             <h1 className='font-bold text-3xl mb-5'>Proyectos</h1>
             <div className='flex-1'>
                 <h3 className='text-xl font-bold'>Mis proyectos</h3>
--- src/components/app/projects/MyProjectsSection.tsx
+++ src/components/app/projects/MyProjectsSection.tsx
@@ -1,12 +1,27 @@
+import {auth} from "@/auth";
+import  {cookies} from "next/headers";
+import {Project} from "@prisma/client";
+import {ProjectCard} from "@/components/app/projects/ProjectCard";
 
-const MY_PROJECTS = [
-    
-]
-
-export function MyProjectsSection() {
+export async function MyProjectsSection() {
+    const session = await auth()
+    const cookieStore = await cookies()
+    const data = await fetch(`http://localhost:3000/api/advisors/${session?.user.id}/projects`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cookie': cookieStore.toString()
+        }
+    })
+    const projects = await data.json()
+    if (!projects) return <p>No hay proyectos</p>
     return (
-        <div className='outline h-full flex-1 w-full p-5'>
-
-       </div>
+        <div className='flex-1 w-full p-5 flex gap-2'>
+            {
+                projects.map((project: Project) => (
+                    <ProjectCard project={project} key={project.id} />
+                ))
+            }
+      </div>
     )
 }
