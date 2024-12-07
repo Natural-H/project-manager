60d9f06c013328bc90d1d8af3b9dad7a3d79f9d8
Natural-H <marco.mmtz@proton.me>
2024-12-07 17:42:19 -0600

    Setting isFinishedState to false by default

---
+++ prisma/migrations/20241207234034_setting_is_finished_to_false/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "keyname" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "funding" DECIMAL NOT NULL,
+    "dateBegin" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dateEnd" DATETIME,
+    "companyId" INTEGER NOT NULL,
+    "isFinished" TEXT NOT NULL DEFAULT 'false',
+    CONSTRAINT "Project_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Project" ("companyId", "dateBegin", "dateEnd", "description", "funding", "id", "isFinished", "keyname", "name") SELECT "companyId", "dateBegin", "dateEnd", "description", "funding", "id", "isFinished", "keyname", "name" FROM "Project";
+DROP TABLE "Project";
+ALTER TABLE "new_Project" RENAME TO "Project";
+CREATE UNIQUE INDEX "Project_keyname_key" ON "Project"("keyname");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
