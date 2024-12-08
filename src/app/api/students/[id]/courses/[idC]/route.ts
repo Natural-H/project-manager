25c748bd4709abff7af3de2c5c9da970abe3a7f1
Natural-H <marco.mmtz@proton.me>
2024-12-07 20:47:13 -0600

    Adding nested route students/courses

---
+++ src/app/api/students/[id]/courses/[idC]/route.ts
@@ -0,0 +1,42 @@
+import {NextRequest, NextResponse} from "next/server";
+import {prisma} from "@/app/prisma";
+import {Prisma} from "@prisma/client";
+import {auth} from "@/auth";
+
+export async function DELETE(request: NextRequest, {params}: { params: Promise<{ id: string, idC: string }> }) {
+    const session = await auth()
+    if (!session) return NextResponse.json({message: "Not authorized"}, {status: 401})
+
+    const parameters = await params
+    const [id, idC] = [Number(parameters.id), Number(parameters.idC)]
+
+    try {
+        await prisma.student.update({
+            where: {
+                id,
+                courses: {
+                    some: {id: idC}
+                }
+            },
+            data: {
+                courses: {
+                    disconnect: {
+                        id: idC
+                    }
+                }
+            }
+        })
+
+        return NextResponse.json(null, {status: 200})
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2025") return NextResponse.json(null, {status: 404})
+
+            return NextResponse.json({message: e.message, code: e.code}, {status: 500})
+        } else if (e instanceof Prisma.PrismaClientValidationError) {
+            return NextResponse.json({message: "Malformed request"}, {status: 400})
+        }
+
+        throw e
+    }
+}
