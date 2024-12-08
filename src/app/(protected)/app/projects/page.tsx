c89f4b3b1fe58b2216b421c6a44e811054dd71bd
esalu-dev <esalu@profedex.top>
2024-12-07 20:21:32 -0600

    Add AllProjectsSection.tsx to Projects Page

--- src/app/(protected)/app/projects/page.tsx
+++ src/app/(protected)/app/projects/page.tsx
@@ -1,14 +1,20 @@
 import {MyProjectsSection} from "@/components/app/projects/MyProjectsSection";
+import {AllProjectsSection} from "@/components/app/projects/AllProjectsSection";
+import {AddProjectButton} from "@/components/app/projects/AddProjectButton";
 
 export default function ProjectsPage() {
     return (
         <div className='h-full flex-1 w-full p-5'>
             <h1 className='font-bold text-3xl mb-5'>Proyectos</h1>
+            <AddProjectButton />
             <div className='flex-1'>
                 <h3 className='text-xl font-bold'>Mis proyectos</h3>
                 <p className='text-sm text-gray-500'>Aquí encontrarás tus proyectos</p>
                 <MyProjectsSection />
-           </div>
+                <h3 className='text-xl font-bold'>Todos los proyectos</h3>
+                <p className='text-sm text-gray-500'>Aquí encontrarás todos los proyectos</p>
+                <AllProjectsSection />
+            </div>
         </div>
     )
 }
---
+++ src/components/app/projects/AllProjectsSection.tsx
@@ -0,0 +1,25 @@
+import  {cookies} from "next/headers";
+import {Project} from "@prisma/client";
+import {ProjectCard} from "@/components/app/projects/ProjectCard";
+
+export async function AllProjectsSection() {
+    const cookieStore = await cookies()
+    const data = await fetch('http://localhost:3000/api/projects', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cookie': cookieStore.toString()
+        }
+    })
+    const projects = await data.json()
+    if (!projects) return <p>No hay proyectos</p>
+    return (
+        <div className='flex-1 w-full p-5 flex gap-2'>
+            {
+                projects.map((project: Project) => (
+                    <ProjectCard project={project} key={project.id} />
+                ))
+            }
+        </div>
+    )
+}
