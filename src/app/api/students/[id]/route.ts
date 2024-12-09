b329dddde7fe0f9ffdc7c3a471b11a096f2fcae9
Marco Mauricio <marco.mmtz@proton.me>
2024-12-09 13:08:07 -0600

    Revert "Adding auth for GET and DELETE on students/[id]"
    This reverts commit 0a16828fa51269e64d70f21b7b8d4c251cff763e.

--- src/app/api/students/[id]/route.ts
+++ src/app/api/students/[id]/route.ts
@@ -4,9 +4,6 @@ import {NextRequest, NextResponse} from "next/server";
 import {Prisma} from "@prisma/client";
 
 export async function GET(request: Request, {params}: { params: Promise<{ id: string }> }) {
-    const session = await auth()
-    if (!session) return NextResponse.json({message: "Not authorized"}, {status: 401})
-
     try {
         const student = await prisma.student.findUnique({
             where: {
@@ -31,8 +28,6 @@ export async function GET(request: Request, {params}: { params: Promise<{ id: st
 }
 
 export async function DELETE(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
-    const session = await auth()
-    if (!session) return NextResponse.json({message: "Not authorized"}, {status: 401})
     const id = Number((await params).id)
 
     try {
